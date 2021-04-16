2384a0063ca6fa67f8cdf04736b032e43c6df32f
Kenneth Bonte <kenneth.bonte@durwella.com>
2021-04-16 12:16:48 -0400

    adding an angular 8 project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'tree-component-upgrade';
+  title = 'Upgrading to Circlon attempt';
 }
